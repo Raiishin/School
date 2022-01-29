20fd9458956823a8f1fbe9fe7431fcf8fe23f7a5
Raiishin <gavin.tanjy11@gmail.com>
2022-01-30 01:15:01 +0800

    Refactor :: Added in comments for documentation

--- CSIT110/Assignment 2/TAN_JUN_YIN_GAVIN_7573935_a2.py
+++ CSIT110/Assignment 2/TAN_JUN_YIN_GAVIN_7573935_a2.py
@@ -19,7 +19,8 @@ def question_1():
         forwardSum = forwardSum + i
         i = i + GAP
 
-    i = UPPERBOUND
+    i = UPPERBOUND  # Reset count to start from UPPERBOUND
+
     # Moving Down
     while i > 0:
         backwards.append(i)
@@ -43,13 +44,12 @@ def question_2():
     combinedSum = sa + ma
     accumulatedInterest = 0
 
-    # Calculate RA Interest
     if AGE >= 55:
+        # Account for AGE >= 55
         ra = int(input('Enter current amount in RA: '))
         combinedSum += ra
 
-    # Calculate OA Interest
-    if AGE >= 55:
+        # Calculating OA Interest
         if oa >= 20000:
             accumulatedInterest += (20000*0.045)
             oa = - 20000
@@ -61,6 +61,7 @@ def question_2():
         else:
             accumulatedInterest += (oa*0.045)
 
+        # Calculating SA + MA + RA Interest
         if combinedSum >= 20000:
             accumulatedInterest += (20000*0.06)
             combinedSum = combinedSum - 20000
@@ -76,38 +77,45 @@ def question_2():
             accumulatedInterest += (combinedSum*0.06)
 
     else:
+        # Calculating OA Interest
         if oa >= 20000:
             accumulatedInterest += (20000*0.035) + ((oa-20000)*0.025)
         else:
             accumulatedInterest += (oa*0.035)
 
+        # Calculating SA + MA Interest
         if combinedSum >= 40000:
             accumulatedInterest += (40000*0.05) + ((combinedSum-20000)*0.04)
         else:
             accumulatedInterest += (combinedSum*0.05)
 
     print(f"Your interest rate this year will be ${accumulatedInterest:.2f}")
-    return accumulatedInterest
 
 
 def question_3():
+
+    # User Inputs
     numberOfSingleRooms = int(input('Number of Single rooms: '))
     numberOfTwinRooms = int(input('Number of Twin rooms: '))
     numberOfDeluxeRooms = int(input('Number of Deluxe rooms: '))
     lengthOfStay = int(input('Length of stay(number of nights): '))
 
+    # Calculating Cost of Rooms
     costOfSingleRooms = numberOfSingleRooms * lengthOfStay * 90
     costOfTwinRooms = numberOfTwinRooms * lengthOfStay * 150
     costOfDeluxeRooms = numberOfDeluxeRooms * lengthOfStay * 250
 
+    # Formatting Cost of Rooms
     formattedCostOfSingleRooms = f"${costOfSingleRooms:.2f}"
     formattedCostOfTwinRooms = f"${costOfTwinRooms:.2f}"
     formattedCostOfDeluxeRooms = f"${costOfDeluxeRooms:.2f}"
 
+    # Calculating Total Rooms and Total Cost
     totalRooms = numberOfSingleRooms + numberOfTwinRooms + numberOfDeluxeRooms
     totalCost = costOfSingleRooms + costOfTwinRooms + costOfDeluxeRooms
     totalCostWithGST = totalCost * 1.07
 
+    # Formatting Total Cost
     formattedTotalCost = f"${totalCost:.2f}"
     formattedTotalCostWithGST = f"${totalCostWithGST:.2f}"
 
@@ -169,9 +177,14 @@ def question_4():
 
 
 def main():
+
+    # Note: print() was added for improved readability
     question_1()
+    print()
     question_2()
+    print()
     question_3()
+    print()
     print(question_4())
 
 
